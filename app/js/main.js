64378e0092deca513db4d3599ee6617f7d128a7f
dfvgbh <kaktusad@gmail.com>
2017-01-08 03:55:09 +0200

    IE8 supporting is shit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
-function ready() {
-  
-}
+/*global
+console, $
+*/
+
+;(function() {
+  'use strict';
+
+  function ready() {
+    // document.scroll not supported in IE8
+    $(window).scroll(function() {
+      $('.nav-wrapper').toggleClass('nav-wrapper-transparent', $(this).scrollTop() < 150);
+    });
+  }
+
+  $(function() {
+    ready();
+  });
+})();
