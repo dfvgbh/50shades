cc280229a3ebad477daa072af7b71b49eb192a45
dfvgbh <kaktusad@gmail.com>
2017-01-14 03:34:32 +0200

    gallery.js refactored, some comments added, lil fixed

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -5,27 +5,60 @@ console, $
 ;(function() {
   'use strict';
 
-  var gallery = $('#gallery');
-  var root = 'http://localhost:3000';
+  var gallery     = $('#gallery'),
+      itemsLimit  = 10,
+      notFoundUrl = 'http://ci.memecdn.com/1850732.jpg',
+      itemHtml    = '<div class="photo-container"><a href="/"class="photo-item"></a></div>',
+      root        = 'http://localhost:3000/images';
 
-  function get(id) {
+  /**
+   * Sends request to get an array of images refered to the @page. Return Promise.
+   */
+  function getPage(page, limit) {
+    limit = typeof limit !== 'undefined' ?  limit : itemsLimit;
+
     return $.ajax({
-      dataType: "json",
-      url: root + '/images/' + id,
-      method: 'GET'
-    }).done(function(data) {
-      addPhoto(data);
+      method: 'GET',
+      dataType: 'json',
+      url: root + '?_page=' + page + '&_limit=' + limit,
+      // TODO
+      beforeSend:function(){
+        gallery.html('<div class="loading"><img src="../images/loading.gif" alt="Loading..." /></div>');
+      }
     });
   }
 
-  function addPhoto(data) {
-    $('<div class="photo-container"><a href="/"class="photo-item"></a></div>').css('background-image', 'url(' + data.url + ')').appendTo(gallery);
+  /**
+   * Injecting and image that represented by JSON object @data into gallery.
+   */
+  function injectItem(data) {
+    data.url = data.url || notFoundUrl;
+    $(itemHtml).css('background-image', 'url(' + data.url + ')').appendTo(gallery);
+  }
+
+  /**
+   * Managing page loading.
+   */
+  function proceedData() {
+    // TODO
+    for (var i = 1; i < 4; i++) {
+      getPage(i)
+      .done(function(data) {
+        $('.loading').remove();
+        if ($.isArray(data)) {
+          for (var j = 0; j < data.length; j++) {
+            injectItem(data[j]);
+          }
+        } else {
+          injectItem(data);
+        }
+      })
+      .fail(function() {
+      });
+    }
   }
 
   $(function() {
-    var o = null;
-    for (var i = 0; i < 20; i++) {
-      get(i);
-    }
+    proceedData();
   });
 })();
