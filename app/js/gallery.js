0d3d7e77686d118d55c7d43563fa57ff14d6cc88
dfvgbh <kaktusad@gmail.com>
2017-01-15 03:46:38 +0200

    gallery.js refactored. Pagination added

--- app/js/gallery.js
+++ app/js/gallery.js
@@ -5,26 +5,65 @@ console, $
 ;(function() {
   'use strict';
 
+    /* * * * * * * * * * * * * * * * *
+   * Managing
+   * image gallery
+   * * * * * * * * * * * * * * * * */
+
   var gallery     = $('#gallery'),
       itemsLimit  = 10,
+      lastPage     = 0,
       notFoundUrl = 'http://ci.memecdn.com/1850732.jpg',
       itemHtml    = '<div class="photo-container"><a href="/"class="photo-item"></a></div>',
       root        = 'http://localhost:3000/images';
 
   /**
-   * Sends request to get an array of images refered to the @page. Return Promise.
+   * Sends request to get an array of images refered to the @page. 
+   * Updating gallery with requested iamges.
+   * @return {Promise} - promise.
    */
-  function getPage(page, limit) {
-    limit = typeof limit !== 'undefined' ?  limit : itemsLimit;
+  function updateGallery(page) {
 
     return $.ajax({
       method: 'GET',
       dataType: 'json',
-      url: root + '?_page=' + page + '&_limit=' + limit,
-      // TODO
+      url: root + '?_page=' + page + '&_limit=' + itemsLimit,
       beforeSend:function(){
-        gallery.html('<div class="loading"><img src="../images/loading.gif" alt="Loading..." /></div>');
+        gallery.empty();
+        gallery.append('<div class="loading"><img src="./images/loading.gif" alt="Loading..." /></div>');
       }
+    })
+    .done(function(data, status, xhr) {      
+      gallery.find('.loading').remove();
+      if ($.isArray(data)) {
+        for (var j = 0; j < data.length; j++) {
+          injectItem(data[j]);
+        }
+      } else {
+        injectItem(data);
+      }
+    })
+    .fail(function() {
+      gallery.html('Sorry, can\'t found');
+    });
+  }
+
+  /**
+   * Updating last page number. Sending ajax request for getting header,
+   * which contains total count of records in base.
+   * @return {Promise} - promise.
+   */
+  function updateLastPageNumber() {
+    return $.ajax({
+      method: 'GET',
+      dataType: 'json',
+      url: root + '?_page=0_limit=1'
+    })
+    .done(function(data, status, xhr) {
+      lastPage = Math.floor((xhr.
+        getResponseHeader('X-Total-Count') - 1) / itemsLimit + 1);
+    })
+    .fail(function() {
     });
   }
 
@@ -36,29 +75,175 @@ console, $
     $(itemHtml).css('background-image', 'url(' + data.url + ')').appendTo(gallery);
   }
 
-  /**
-   * Managing page loading.
-   */
-  function proceedData() {
-    // TODO
-    for (var i = 1; i < 4; i++) {
-      getPage(i)
-      .done(function(data) {
-        $('.loading').remove();
-        if ($.isArray(data)) {
-          for (var j = 0; j < data.length; j++) {
-            injectItem(data[j]);
-          }
-        } else {
-          injectItem(data);
+
+  /* * * * * * * * * * * * * * * * *
+   * Pagination
+   * javascript page navigation
+   * * * * * * * * * * * * * * * * */
+
+  var Pagination = {
+
+    html: '',
+
+    // converting initialize data
+    Extend: function(data) {
+      data = data || {};
+      Pagination.size = data.size || 300;
+      Pagination.page = data.page || 1;
+      Pagination.step = data.step || 3;
+      Pagination.callback = function() {
+        if (typeof(data.callback) === typeof(Function)) {
+          var args = Array.prototype.slice.call(arguments);
+          data.callback(args);
         }
-      })
-      .fail(function() {
-      });
+      };
+    },
+
+    // add pages by number (from [s] to [f])
+    Add: function(s, f) {
+      for (var i = s; i < f; i++) {
+          Pagination.html += '<a>' + i + '</a>';
+      }
+    },
+
+    // add last page with separator
+    Last: function() {
+      Pagination.html += '<i>...</i><a>' + Pagination.size + '</a>';
+    },
+
+    // add first page with separator
+    First: function() {
+      Pagination.html += '<a>1</a><i>...</i>';
+    },
+
+    // --------------------
+    // Handlers
+    // --------------------
+
+    // change page
+    Click: function(e) {
+      
+      Pagination.page = +this.innerHTML;
+      Pagination.Start();
+    },
+
+    // previous page
+    Prev: function() {
+      Pagination.page--;
+      if (Pagination.page < 1) {
+          Pagination.page = 1;
+      }
+      Pagination.Start();
+    },
+
+    // next page
+    Next: function() {
+      Pagination.page++;
+      if (Pagination.page > Pagination.size) {
+          Pagination.page = Pagination.size;
+      }
+      Pagination.Start();
+    },
+
+
+
+    // --------------------
+    // Script
+    // --------------------
+
+    // binding pages
+    Bind: function() {
+      var a = Pagination.e.getElementsByTagName('a');
+      for (var i = 0; i < a.length; i++) {
+          if (+a[i].innerHTML === Pagination.page) a[i].className = 'current';
+          a[i].addEventListener('click', Pagination.Click, false);
+      }
+    },
+
+    // write pagination
+    Finish: function() {
+      Pagination.e.innerHTML = Pagination.html;
+      Pagination.html = '';
+      Pagination.Bind();
+    },
+
+    // find pagination type
+    Start: function() {
+      Pagination.callback(Pagination.page);
+
+      if (Pagination.size < Pagination.step * 2 + 6) {
+          Pagination.Add(1, Pagination.size + 1);
+      }
+      else if (Pagination.page < Pagination.step * 2 + 1) {
+          Pagination.Add(1, Pagination.step * 2 + 4);
+          Pagination.Last();
+      }
+      else if (Pagination.page > Pagination.size - Pagination.step * 2) {
+          Pagination.First();
+          Pagination.Add(Pagination.size - Pagination.step * 2 - 2, Pagination.size + 1);
+      }
+      else {
+          Pagination.First();
+          Pagination.Add(Pagination.page - Pagination.step, Pagination.page + Pagination.step + 1);
+          Pagination.Last();
+      }
+      Pagination.Finish();
+    },
+
+
+
+    // --------------------
+    // Initialization
+    // --------------------
+
+    // binding buttons
+    Buttons: function(e) {
+      var nav = e.getElementsByTagName('a');
+      nav[0].addEventListener('click', Pagination.Prev, false);
+      nav[1].addEventListener('click', Pagination.Next, false);
+    },
+
+    // create skeleton
+    Create: function(e) {
+      var html = [
+          '<a>&#9668;</a>', // previous button
+          '<span></span>',  // pagination container
+          '<a>&#9658;</a>'  // next button
+      ];
+
+      e.innerHTML = html.join('');
+      Pagination.e = e.getElementsByTagName('span')[0];
+      Pagination.Buttons(e);
+    },
+
+    // init
+    Init: function(e, data) {
+      Pagination.Extend(data);
+      Pagination.Create(e);
+      Pagination.Start();
     }
-  }
+  };
+
+
+
+  /* * * * * * * * * * * * * * * * *
+  * Initialization
+  * * * * * * * * * * * * * * * * */
+
+  var init = function() {
+    updateLastPageNumber()
+    .done(function() {
+      Pagination.Init(document.getElementById('pagination'), {
+          size: lastPage,
+          page: 1,
+          step: 3,
+          callback: updateGallery
+      });
+    });
+  };
+
 
   $(function() {
-    proceedData();
+    init();
   });
 })();
